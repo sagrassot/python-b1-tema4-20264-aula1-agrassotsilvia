6d855db7cbe2f5819d82abbc802f91f7732c86af
Silvia Agrassot <sagrassot@gmail.com>
2026-05-20 07:20:46 +0200

    Ej4a1.py Acabat

--- 4a/ej4a1.py
+++ 4a/ej4a1.py
@@ -38,12 +38,12 @@ list_2 = [4, 5, 6, 7, 8]
 
 
 def find_intersection(list_1, list_2):
-    # Write here your code
+    return list(set(list_1) & set(list_2))
     pass
 
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
 
-# print(find_intersection([1, 2, 3, 4], [3, 4, 5, 6]))
-# print(find_intersection(['apple', 'banana', 'orange'], ['banana', 'kiwi', 'apple']))
+print(find_intersection([1, 2, 3, 4], [3, 4, 5, 6]))
+print(find_intersection(['apple', 'banana', 'orange'], ['banana', 'kiwi', 'apple']))
