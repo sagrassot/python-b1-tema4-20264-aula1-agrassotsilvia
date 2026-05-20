ac0f3316f208d596f891758bb662f398bc7c9ff7
Silvia Agrassot <sagrassot@gmail.com>
2026-05-20 07:26:22 +0200

    eja2.py Acabat

--- 4a/ej4a2.py
+++ 4a/ej4a2.py
@@ -44,11 +44,17 @@ Exemple:
 
 
 def count_fruits(fruits_list):
-    # Write here your code
+    fruit_count = {}
+    for fruit in fruits_list:
+        if fruit in fruit_count:
+            fruit_count[fruit] +=1
+        else:
+            fruit_count[fruit] = 1
+    return fruit_count
     pass
 
 # Si quieres probar tu código, descomenta las siguientes líneas y ejecuta el script
 # Si vols provar el teu codi, descomenta les línies següents i executa l'script
 
-# fruits = ['apple', 'banana', 'orange', 'apple', 'kiwi', 'banana', 'kiwi', 'kiwi', 'kiwi']
-# print(count_fruits(fruits))
+fruits = ['apple', 'banana', 'orange', 'apple', 'kiwi', 'banana', 'kiwi', 'kiwi', 'kiwi']
+print(count_fruits(fruits))
